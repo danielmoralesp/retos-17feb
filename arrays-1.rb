ab59c65009f4b2c930116da1b0dc3162f6306bac
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-17 18:38:15 -0500

    primer ejericio resuelto

---
+++ arrays-1.rb
@@ -0,0 +1,5 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+zombie_apocalypse_supplies.each do |element|
+  puts element
+end
