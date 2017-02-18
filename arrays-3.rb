18d9bcc98ef4c6013e865f5936a6768e200985d8
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-17 19:01:32 -0500

    reto 3 resuelto

---
+++ arrays-3.rb
@@ -0,0 +1,6 @@
+def item(array)
+  zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+  zombie_apocalypse_supplies.include?(array)
+end
+
+item("hatchet")
