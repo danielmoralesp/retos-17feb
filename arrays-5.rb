99094056e8e3362654ccd202aaf4bf2aa66e96c3
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-17 19:53:06 -0500

    ejercicio 5 resuelto

---
+++ arrays-5.rb
@@ -0,0 +1,5 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove", "solar battery", "flashlight" ]
+
+merge = zombie_apocalypse_supplies + other_survivor_supplies
+merge.uniq
