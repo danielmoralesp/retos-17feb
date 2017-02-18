b8c65cc2684dbe57b5332b0db6739477913e4f18
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-17 20:00:09 -0500

    ejercicio hashes 1 resuelto

---
+++ hashes-1.rb
@@ -0,0 +1,13 @@
+extinct_animals = {
+  "Tasmanian Tiger" => 1936,
+  "Eastern Hare Wallaby" => 1890,
+  "Dodo" => 1662,
+  "Pyrenean Ibex" => 2000,
+  "Passenger Pigeon" => 1914,
+  "West African Black Rhinoceros" => 2011,
+  "Laysan Crake" => 1923
+}
+
+extinct_animals.each do |key, value|
+  puts "* #{key} - #{value}"
+end
