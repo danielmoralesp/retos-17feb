59030c75e729a9d5dcb897167629121e4a2efd85
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-17 20:35:29 -0500

    ejercicio 3 de hashes realizado

---
+++ hashes-3.rb
@@ -0,0 +1,11 @@
+extinct_animals = {
+  "Tasmanian Tiger" => 1936,
+  "Eastern Hare Wallaby" => 1890,
+  "Dodo" => 1662,
+  "Pyrenean Ibex" => 2000,
+  "Passenger Pigeon" => 1914,
+  "West African Black Rhinoceros" => 2011,
+  "Laysan Crake" => 1923
+}
+
+extinct_animals.update(extinct_animals){ |k,v| v - 3 }
