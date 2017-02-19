39fa99687073334502c16f4324d10e244c107b02
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-19 15:33:45 -0500

    con angie methods. Gracias angie

--- nested-3.rb
+++ nested-3.rb
@@ -1,3 +1,4 @@
 nested_data = {array: ["array", {hash: "finished"}]}
 
 puts nested_data[:array][1][:hash]
+#prueba
--- nested-4.rb
+++ nested-4.rb
@@ -1,3 +1,4 @@
 number_array = [5, [10, 15], [20,25,30], 35]
 
 number_array.flatten.collect{ |x| x + 5}
+#prueba
